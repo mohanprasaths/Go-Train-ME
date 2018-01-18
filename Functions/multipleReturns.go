f9584ecf1ce31b4e7229847d442bbe6ede61cbae
mohanprasaths <mohanprasath@live.com>
2018-01-18 16:47:47 +0530

    Variadic function implemented

--- Functions/multipleReturns.go
+++ Functions/multipleReturns.go
@@ -12,11 +12,22 @@ func Printme(myGreetings Greetings) {
 	formal, informal := GreetingsConvertor(myGreetings)
 	fmt.Println(formal)
 	fmt.Println(informal)
+	formal, informal, variaDic := variaDicGreetingsConvertor(myGreetings, "working", "  fine")
+	fmt.Println(formal)
+	fmt.Println(informal)
+	fmt.Println(variaDic)
 }
 
 func GreetingsConvertor(myGreetings Greetings) (formalWish, informalWish string) {
 	formalWish = myGreetings.greeting + "  " + myGreetings.name
 	informalWish = myGreetings.informalGreet + "  " + myGreetings.name
+	return
+}
+
+func variaDicGreetingsConvertor(myGreetings Greetings, variaDic ...string) (formalWish, informalWish, variaDicEg string) {
+	formalWish = myGreetings.greeting + "  " + myGreetings.name
+	informalWish = myGreetings.informalGreet + "  " + myGreetings.name
+	variaDicEg = variaDic[0] + variaDic[1]
 	return
 }
 
